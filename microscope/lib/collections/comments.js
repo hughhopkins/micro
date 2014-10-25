ef687636183305c98d702e52305aafd8ef6f772b
Hugh Hopkins <hugh.hopkins@gmail.com>
2014-10-25 14:55:28 +0100

    Commit 10-5

--- microscope/lib/collections/comments.js
+++ microscope/lib/collections/comments.js
@@ -1,21 +1,21 @@
 Comments = new Mongo.Collection('comments');
 
 Meteor.methods({
-	commentInsert: function(commentAtributes) {
-		check(this.userId, String);
-		check(commentAtributes, {
-			postId: String,
-			body: String
-		});
-		var user = Meteor.user();
-		var post = Posts.findOne(commentAtributes.postId);
-		if (!post)
-			throw new Meteor.Error('invalid-comment', 'you must comment on a post');
-		comment = _.extend(commentAtributes, {
-			userId: user._id,
-			author: user.username,
-			submitted: new Date()
-		});
-		return Comments.insert(comment);
-	}
+  commentInsert: function(commentAttributes) {
+    check(this.userId, String);
+    check(commentAttributes, {
+      postId: String,
+      body: String
+    });
+    var user = Meteor.user();
+    var post = Posts.findOne(commentAttributes.postId);
+    if (!post)
+      throw new Meteor.Error('invalid-comment', 'You must comment on a post');
+    comment = _.extend(commentAttributes, {
+      userId: user._id,
+      author: user.username,
+      submitted: new Date()
+    });
+    return Comments.insert(comment);
+  }
 });

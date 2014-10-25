1ed8f6a1ef18d646e379e801e12a1f4bb93973cd
Hugh Hopkins <hugh.hopkins@gmail.com>
2014-10-25 11:57:03 +0100

    updating to .0.4 with bootstrap 3 and Mondo collection

--- microscope/lib/permissions.js
+++ microscope/lib/permissions.js
@@ -1,4 +1,4 @@
-// check the the userId specified owns the documents
+// check that the userId specified owns the documents
 ownsDocument = function(userId, doc) {
   return doc && doc.userId === userId;
 }

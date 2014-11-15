d1872ff0c19649f6e74ae5ddfebe7a0fce06b566
Hugh Hopkins <hugh.hopkins@gmail.com>
2014-11-15 14:31:00 +0000

    Commit 16-4

--- microscope/packages/bitly/bitly.js
+++ microscope/packages/bitly/bitly.js
@@ -21,3 +21,28 @@ Bitly.shortenURL = function(url) {
 		throw new Meteor.Error(500, "Bitly call failed with an error of some sort, maybe: "+shoretenResponse.status_txt);
 	}
 }
+
+Bitly.getClicks = function(link){
+	if(!Meteor.settings.bitly)
+		throw new Meteor.Error(500, 'please pr a Bitly thoken in Meteor.settings');
+
+	var statsResponse = Meteor.http.get(
+		"https://api-ssl.bitly.com/v3/link/clicks?",
+		{
+			timeout: 5000,
+			params:{
+				"format": "json",
+				"access_token": Meteor.settings.bitly,
+				"link": link
+			}
+		}
+	);
+	if(statsResponse.data.status_code === 200)
+		return statsResponse.data.data.link_clicks
+}
+
+Meteor.methods({
+	'getBitlyClicks': function(link){
+		return Bitly.getClicks(link);
+	}
+});

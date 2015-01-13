1f51fb03bb116d8e9bc9a4a41b0d92c1207e96e9
hugh <hugh@hughs-iMac.local>
2015-01-13 18:41:39 +0000

    add GoSquared custom data

--- microscope/client/main.js
+++ microscope/client/main.js
@@ -22,7 +22,17 @@ Tracker.autorun(function(){
 		  name: Meteor.user().username,
 		  email: Meteor.user().emails[0].address,
 		  createdAt: Meteor.user().createdAt,
-		  phone: Math.floor((Math.random() * 100000000) + 1)
+		  phone: Math.floor((Math.random() * 100000000) + 1),
+		  company_size: 12,
+		  custom: { 
+		  	numberPosts: 79,
+		  	test1: 8393,
+		  	MRR: 1000000000,
+		  	location: "London",
+		  	simon: 99,
+		  	site: "https://www.gosquared.com/",
+		  	personalSite: "http://hughhopkins.meteor.com"
+		   }
 		};
 
 		_gs('identify', userID, user);
